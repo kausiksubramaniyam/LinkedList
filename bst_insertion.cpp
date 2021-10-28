feb4dc96f2e565d51e630a496e50e08b45aa757b
kausiksubramaniyam <59435311+kausiksubramaniyam@users.noreply.github.com>
2021-10-28 22:54:50 +0530

    Create bst_insertion.cpp

---
+++ bst_insertion.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+struct bst{
+    int data;
+    struct bst* left=NULL;
+    struct bst* right=NULL;
+};
+void createtree(int val,struct bst* root){
+    if(val<root->data){
+        if(root->left==NULL){
+            struct bst* newbst=(struct bst*)malloc(sizeof(struct bst));
+            newbst->data=val;
+            root->left=newbst;
+        }
+        else{
+            createtree(val,root->left);
+        }
+    }
+    else{
+        if(root->right==NULL){
+            struct bst* newbst=(struct bst*)malloc(sizeof(struct bst));
+            newbst->data=val;
+            root->right=newbst;
+        }
+        else{
+            createtree(val,root->right);
+        }
+    }
+}
+int main(){
+    int i,n,val;
+    cout<<"Enter no.of nodes required:";
+    cin>>n;
+    struct bst* root=(struct bst*)malloc(sizeof(struct bst));
+    cout<<"Enter root value:";
+    cin>>root->data;
+    for(i=0;i<n;i++){
+        cout<<"Enter value:";
+        cin>>val;
+        createtree(val,root);
+    }
+    return 0;
+}
